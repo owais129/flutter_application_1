80aaa46295e8c95fe4165a356c4efab97757c7d4
owais129 <134064317+owais129@users.noreply.github.com>
2023-08-29 04:29:18 -0700

    Delete New folder/New folder directory

--- New folder/New folder/lib/model/category_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CategoryModel {
-  final String title;
-  final IconData icon;
-  final bool active;
-  CategoryModel(
-      {required this.title, required this.icon, required this.active});
-}
